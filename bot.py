ad68c8542083d6d4c492534b10ad5ecbc99f225c
SMN947 <betum98@gmail.com>
2021-10-09 18:43:36 -0500

    Modules loader, removed default help command

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 import discord
 from itertools import cycle
 from discord.ext import commands, tasks
+import os
 
 with open("config.json",'r') as f:
     CONFIG = json.load(f)
@@ -12,6 +13,8 @@ INTENTS = discord.Intents.default()
 BOT = commands.Bot(command_prefix=PREFIX, intents=INTENTS)
 STATUS = cycle(['Try * help','Prefix - *'])
 
+BOT.remove_command("help")
+
 @BOT.event
 async def on_ready():
     change_status.start()
@@ -20,5 +23,9 @@ async def on_ready():
 @tasks.loop(seconds=5)
 async def change_status():
     await BOT.change_presence(activity=discord.Game(next(STATUS)))
+
+for filename in os.listdir('./modulos'):
+    if filename.endswith('.py'):
+        BOT.load_extension(f'modulos.{filename[:-3]}')
 
 BOT.run(TOKEN)
